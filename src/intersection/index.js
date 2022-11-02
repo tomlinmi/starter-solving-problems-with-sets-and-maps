cfbd9e1ffc1214bcaf3d5ca61dc091a514f42619
Michelle <mntomlin1@gmail.com>
2022-11-02 19:51:02 -0400

    problems solved

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,22 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+const _map = new Map();
+const _set = new Set();
+
+
+for (const element of a){
+_map.set(element, element);
+}
+
+for(const element of b){
+if(_map.has(element)){
+
+
+    _set.add(element);
+}
+}
+ return ([..._set]);
+
+
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -1,8 +1,55 @@
+/*Given an array of distinct numbers, you wish to find all pairs of numbers in the array that sum to a given number.*/
+
+/*inputs: an array of numbers named A
+        a target value named N
+
+initialize a new Map named numbers
+initialize a new Map named solution
+
+for each element e in A do:
+   add e to numbers
+
+for each element e in A do;
+  calculate diff = N - e
+  if diff is not equal to e then  (1)
+     look up diff in numbers
+     if diff is in numbers then
+       add [e, diff] to solution (2)
+
+convert solution to an array and return it
+*/
+
+
+
+
 /**
  * Return an array of pairs of numbers that sum to N
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+
+const numbers = new Map ();
+const solution = new Map();
+
+for(let e of A){
+numbers.set(e);
+}
+
+for (let e of A){
+    let diff = N-e;
+
+if ((diff!==e)){
+if (numbers.has(diff)){
+solution.set(Math.min(e, diff), Math.max(e, diff))
+
+}
+}
+
+}
+return ([...solution]);
+}
+
+
 
 module.exports = sumPairs;
